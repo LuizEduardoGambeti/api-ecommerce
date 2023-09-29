44e0a5caa3e8307f4393f21b6496197e02e40b59
Gambeti <Gambeti.KG@gmail.com>
2023-09-29 02:54:54 -0300

    project create

---
+++ src/main/java/gambeti/github/com/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package gambeti.github.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
